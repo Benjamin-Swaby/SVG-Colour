e9e926a795d92183578f0aa252625d22ef6b0949
ben <benjaminswaby09@gmail.com>
2021-06-14 22:45:49 +0100

    Trying to remove the thing

--- program/target/debug/deps/program-324a8229e85f8b0a.d
+++
@@ -1,8 +0,0 @@
-/home/ben/projects/SVG-Colour/program/target/debug/deps/program-324a8229e85f8b0a.rmeta: src/main.rs src/load.rs src/search.rs src/colours.rs
-
-/home/ben/projects/SVG-Colour/program/target/debug/deps/program-324a8229e85f8b0a.d: src/main.rs src/load.rs src/search.rs src/colours.rs
-
-src/main.rs:
-src/load.rs:
-src/search.rs:
-src/colours.rs:
--- program/target/debug/deps/program-c6fee1b002a6fdcf.d
+++
@@ -1,8 +0,0 @@
-/home/ben/projects/SVG-Colour/program/target/debug/deps/program-c6fee1b002a6fdcf.rmeta: src/main.rs src/load.rs src/search.rs src/colours.rs
-
-/home/ben/projects/SVG-Colour/program/target/debug/deps/program-c6fee1b002a6fdcf.d: src/main.rs src/load.rs src/search.rs src/colours.rs
-
-src/main.rs:
-src/load.rs:
-src/search.rs:
-src/colours.rs:
